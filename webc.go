e3edadcebdc08f976f7195352535aa9bb6e1403d
whithey870106@gmail.com <whithey870106@gmail.com>
2017-08-12 17:25:55 +0800

    yangxu add simget & simpost

---
+++ webc.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func SimGet(requestUrl string) (respHttpCode int, respBody string, err error) {
+
+	req, err := http.NewRequest("GET", requestUrl, nil)
+	if err != nil {
+		return
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		return
+	}
+
+	respHttpCode = resp.StatusCode
+	respBody = string(body)
+
+	return
+}
+
+func SimPost(reqeustUrl string, postData byte) (respHttpCode int, respBody sring, err error) {
+
+	req, err := http.NewRequest("POST", requestUrl, postData)
+	if err != nil {
+		return
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	respHttpCode = resp.StatusCode
+	respBody = string(body)
+
+	return
+}
